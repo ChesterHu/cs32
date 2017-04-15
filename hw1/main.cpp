807c664b49905cff19321ee2b9a62cf965a45d4a
Chufeng <huchufeng@g.ucla.edu>
2017-04-14 21:19:44 -0700

    Add excess header file for testing

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "Set.h"
+#include "Set.h"	// For test
 #include "SSNSet.h"
+#include "SSNSet.h"	// For test
 //#include "test.h"
 #include "testSSNSet.h"
 using namespace std;
