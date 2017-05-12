752e8f9acb073f6d95b0075cd3823bd1af104613
Chufeng <huchufeng@g.ucla.edu>
2017-05-12 16:58:08 -0700

    fix bug in erase

--- hw4/bad.cpp
+++ hw4/bad.cpp
@@ -79,13 +79,17 @@ void removeBad(list<Movie*>& li)
 {
 	list<Movie*>::iterator iter = li.begin();
 	Movie* p;
-	for (; iter != li.end(); ++iter)
+	while (iter != li.end())
 	{
 		if ((*iter)->rating() < 50)
 		{
 			p = *iter;
 			iter = li.erase(iter);
 			delete p;
+		}
+		else
+		{
+			iter++;
 		}
 	}
 }
