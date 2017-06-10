4e6a77b325999b31048d899199a5d188ade67063
Chufeng <huchufeng@g.ucla.edu>
2017-06-10 00:04:42 -0700

    fix typos

--- notes/test.cpp
+++ notes/test.cpp
@@ -19,8 +19,15 @@ class student
 		int id;
 };
 
+int& compare(int& a, int& b)
+{
+	return (a < b) ? a : b;
+}
+
 int main() {
 	student a(2, 8);
 	cout << a.get_age() << endl;
+	int i = 2, j = 3;
+	compare(i, j);
 	return 0;
 }
